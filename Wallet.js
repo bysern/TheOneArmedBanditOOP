c033e1323e79bb03838b72ca4908701672b7d12e
Bysern <ernestbystrzycki@gmail.com>
2021-11-23 15:49:33 +0100

    adding wallet class and dividing scripts in OOP

---
+++ Wallet.js
@@ -0,0 +1,29 @@
+class Wallet{
+    constructor(money){
+        let _money = money;
+
+        this.getCurrentWalletValue = () => _money;
+
+        this.checkIfUserCanPlay = (value) => {
+            if(_money >= value) return true;
+            return false;
+        }
+
+        this.changeWallet = (amount, type="+") =>{
+            if(typeof amount === "number" && !isNaN(amount)){
+                if(type ==="+"){
+                    return _money += amount;
+                }else if(type ==="-"){
+                    return _money -= amount;
+                }else{
+                    throw new Error("Invalid operation type");
+                }
+            } else{
+                console.log(typeof amount);
+                throw new Error("Invalid input")
+            }
+        }
+
+    }
+}
+const wallet = new Wallet(200);
