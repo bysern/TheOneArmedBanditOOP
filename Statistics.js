7d03c668641cd9800d7f303b4241dbaab0e92bc0
Bysern <ernestbystrzycki@gmail.com>
2021-11-24 00:49:34 +0100

    adding game class logic with minor changes

--- Statistics.js
+++ Statistics.js
@@ -1,6 +1,6 @@
 class Statstics{
     constructor(){
-        this.gameResults = [{win: true, bid: 2}, {win: false, bid: -10}];
+        this.gameResults = [];
     }
 
     addGameToStatistics(win, bid){
---
+++ game.js
@@ -0,0 +1,61 @@
+class Game {
+    constructor(start){
+        this.stats = new Statstics();
+        this.wallet = new Wallet(start);
+        document.getElementById('start').addEventListener('click', this.startGame.bind(this));
+        this.spanWallet = document.querySelector('.panel span.wallet');
+        this.boards = [...document.querySelectorAll('div.color')];
+        this.inputBid = document.getElementById('bid');
+        this.spanResult = document.querySelector('.score span.result');
+        this.spanGames = document.querySelector('.score span.number');
+        this.spanWins = document.querySelector('.score span.win');
+        this.spanLosses = document.querySelector('.score span.loss');
+
+        this.render();
+    }
+
+    //default amount is current wallet value
+    render(colors = ['grey','grey','grey'],
+        amount = this.wallet.getCurrentWalletValue(),
+         result = "", stats = [0, 0, 0], bid = 0, wonMoney = 0){
+
+        this.boards.forEach((board, i) =>{
+            board.style.backgroundColor = colors[i];
+        });
+        
+        this.spanWallet.textContent = amount;
+        if(result){
+            result = `Nice! You have won ${wonMoney}$. `;
+        }else if (!result && result !== ""){
+            result = `Bad luck :( You have lost ${bid}$. `;
+        }
+        this.spanResult.textContent = result;
+        this.spanGames.textContent = stats[0];
+        this.spanWins.textContent = stats[1];
+        this.spanLosses.textContent = stats[2];
+
+        this.inputBid.value = "";
+    }
+
+    startGame(){
+        if(this.inputBid.value < 1 ) return alert('The amount is too small.');
+        const bid = Math.floor(this.inputBid.value);
+
+        if(!this.wallet.checkIfUserCanPlay(bid)){
+            return alert('You have too little funds or wrong input');
+        }
+
+        this.wallet.changeWallet(bid, '-');
+
+        this.draw = new Draw();
+        const colors = this.draw.getDrawResult();
+        const win = Result.checkWinner(colors);
+        const wonMoney = Result.moneyWonInGame(win, bid);
+        this.wallet.changeWallet(wonMoney);
+        this.stats.addGameToStatistics(win, bid);
+
+        this.render(colors, this.wallet.getCurrentWalletValue(), win,
+        this.stats.showGameStatistics(), bid, wonMoney);
+    }
+}
+
